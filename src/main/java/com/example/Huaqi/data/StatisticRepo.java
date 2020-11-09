79fc8948c8d27d6a9a70f1934000dbdae128eaea
ChenYZ <181250015@smail.nju.edu.cn>
2020-11-09 20:54:49 +0800

    feat: 完成了获取对冲期权的接口，数据接口加上了参数检验

--- src/main/java/com/example/Huaqi/data/StatisticRepo.java
+++ src/main/java/com/example/Huaqi/data/StatisticRepo.java
@@ -5,20 +5,29 @@ import com.example.Huaqi.po.OptionStatisticPO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public class StatisticRepo {
     @Autowired
     private StatisticMapper statisticMapper;
+    private static final String SECOND_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final String DAY_FORMAT = "yyyy-MM-dd";
 
     /**
-     * 获取某时刻某期权的卖一价、卖一量、买一价、买一量、行权价
-     *
+     * 获取某时刻ETF的卖一价、卖一量、买一价、买一量、行权价
      * @param time yyyy-MM-dd HH:mm:ss格式，最后一位没用，10s为单位
      * @return
      */
-    public ETFStatisticPO getCurrentETFStatistic(String time) {
+    public ETFStatisticPO getCurrentETFStatistic(String time) throws Exception {
+        if(!isLegalDate(time,SECOND_FORMAT)){
+            throw new Exception("Invalid time input，yyyy-MM-dd HH:mm:ss expected.");
+        }
         String table = "daily_statistics@" + time.substring(0, 10);
         String timeStamp = time;
         return statisticMapper.getETFStatisticPerTenSeconds(table, timeStamp);
@@ -29,7 +38,10 @@ public class StatisticRepo {
      * @param time yyyy-MM-dd HH:mm:ss格式，最后一位没用，10s为单位
      * @return
      */
-    public List<OptionStatisticPO> getNegTimeValueOptions(String time) {
+    public List<OptionStatisticPO> getNegTimeValueOptions(String time) throws Exception {
+        if(!isLegalDate(time,SECOND_FORMAT)){
+            throw new Exception("Invalid time input，yyyy-MM-dd HH:mm:ss expected.");
+        }
         String table = "daily_statistics@" + time.substring(0, 10);
         String timeStamp = time.substring(0, time.length() - 1) + "%";
         return statisticMapper.getOptionStatisticPerTenSeconds(table, timeStamp);
@@ -41,7 +53,10 @@ public class StatisticRepo {
      * @param optionCode xxxxxxx.SH
      * @return
      */
-    public OptionStatisticPO getOptionStastic(String time,String optionCode){
+    public OptionStatisticPO getOptionStatistic(String time,String optionCode) throws Exception {
+        if(!isLegalDate(time,SECOND_FORMAT)){
+            throw new Exception("Invalid time input，yyyy-MM-dd HH:mm:ss expected.");
+        }
         String table = "daily_statistics@" + time.substring(0, 10);
         String timeStamp = time.substring(0, time.length() - 1) + "%";
         String[] tmp = optionCode.split("\\.");
@@ -54,7 +69,10 @@ public class StatisticRepo {
      * @param time yyyy-MM-dd HH:mm:ss格式，或者yyyy-MM-dd，都可以
      * @return
      */
-    public Double getETFBid1Earliest(String time){
+    public Double getETFBid1Earliest(String time) throws Exception {
+        if(!(isLegalDate(time,SECOND_FORMAT)||isLegalDate(time,DAY_FORMAT))){
+            throw new Exception("Invalid time input，yyyy-MM-dd HH:mm:ss expected.");
+        }
         String timeStamp = time.substring(0,10);
         String table = "daily_statistics@"+timeStamp;
         return statisticMapper.getEarliestETFBid1(table);
@@ -63,15 +81,64 @@ public class StatisticRepo {
     /**
      * 获取对冲期权
      * 就是获取下个月到期的 delta最小的 认沽期权
-     * TODO:【还没拿到Delta表的数据，这个接口暂时用不了】
-     * @param time yyyy-MM-dd HH:mm:ss格式，或者yyyy-MM-dd，都可以
+     * @param time yyyy-MM-dd HH:mm:ss格式，最后一位没用，10s为单位
      * @return
      */
-    public OptionStatisticPO getHedgeOption(String time){
+    public OptionStatisticPO getHedgeOption(String time) throws Exception {
+        if(!isLegalDate(time,SECOND_FORMAT)){
+            throw new Exception("Invalid time input，yyyy-MM-dd HH:mm:ss expected.");
+        }
         String timeStamp = time.substring(0,10);
         int month = Integer.parseInt(timeStamp.split("\\-")[1]);
-        month=month==12?1:month+1;
-        String fileName = "50ETF沽"+month+"%";
-        return null;
+        int year = Integer.parseInt(timeStamp.split("\\-")[0]);
+        if(month==12){
+            month=1;
+            year+=1;
+        }else{
+            month+=1;
+        }
+
+        String timeQuery = month<10?year+"-0"+month+"%":year+"-"+month+"%";
+        String lvlTable = "daily_lvl_data@"+timeStamp;
+        String setTable = "daily_options_set@"+timeStamp;
+        String code = statisticMapper.getHedgeOptionCode(lvlTable,setTable,timeQuery);
+        System.out.println("hedgeOptionCode="+code);
+        return getOptionStatistic(time,code);
+    }
+
+    /**
+     * 判断输入是否合法
+     * @param date
+     * @param pattern
+     * @return
+     */
+    private boolean isLegalDate(String date,String pattern) {
+        if(pattern.equals(SECOND_FORMAT)) {
+            //用于指定 日期/时间 模式
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern);
+            boolean flag = true;
+            try {
+                //Java 8 新添API 用于解析日期和时间
+                LocalDateTime.parse(date, dtf);
+            } catch (Exception e) {
+                flag = false;
+            }
+            return flag;
+        }else if(pattern.equals(DAY_FORMAT)){
+            int legalLen = 10;
+            if ((date == null) || (date.length() != legalLen)) {
+                return false;
+            }
+
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            try {
+                Date d = formatter.parse(date);
+                return date.equals(formatter.format(d));
+            } catch (Exception e) {
+                return false;
+            }
+        }else {
+            return false;
+        }
     }
 }
--- src/test/java/com/example/Huaqi/test.java
+++ src/test/java/com/example/Huaqi/test.java
@@ -1,18 +1,59 @@
 package com.example.Huaqi;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class test {
+
+    private static final String SECOND_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final String DAY_FORMAT = "yyyy-MM-dd";
+
     public static void main(String[] args) {
-        String name = "50ETF沽4月2.35@10002420.csv";
-        System.out.println('沽'==name.charAt(5));
+//        String name = "50ETF沽4月2.35@10002420.csv";
+//        System.out.println('沽'==name.charAt(5));
 //        String name = "10002287.SH";
 //        for(String s:name.split("\\.")){
 //            System.out.println(s);
 //        }
-
-
 //        String time = "2020-03-25 00:00:00";
 //        String timeStamp = time.substring(0,10);
 //        int month = Integer.parseInt(timeStamp.split("\\-")[1]);
 //        System.out.println(month);
+        System.out.println(test.isDateVail("2020-09-11 23:00:05",SECOND_FORMAT));
+        System.out.println(test.isDateVail("2020-09-11 23:00:05",DAY_FORMAT));
+        System.out.println(test.isDateVail("2020-09-11",DAY_FORMAT));
+        System.out.println(test.isLegalDate("2020-09-11 23:00:05"));
     }
+
+    public static boolean isDateVail(String date,String pattern) {
+        //用于指定 日期/时间 模式
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern);
+        boolean flag = true;
+        try {
+            //Java 8 新添API 用于解析日期和时间
+            LocalDateTime.parse(date, dtf);
+        } catch (Exception e) {
+            flag = false;
+        }
+        return flag;
+    }
+
+    private static boolean isLegalDate(String sDate) {
+        int legalLen = 10;
+        if ((sDate == null) || (sDate.length() != legalLen)) {
+            return false;
+        }
+
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = formatter.parse(sDate);
+            return sDate.equals(formatter.format(date));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
